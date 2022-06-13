c9cff9f51040ce24ab99186ae50bca6a34908c29
Jan Zalewski <jasiozetantispam@gmail.com>
2022-06-13 20:35:06 +0200

    adding the tableu logic, little cleanup

--- src/Components.fs
+++ src/Components.fs
@@ -6,7 +6,7 @@ open Logic
 
 type Components =
   [<ReactComponent>]
-  static member Card(cardLogic, cardIsPicked) =
+  static member StaticCard(cardLogic) =
     Html.div [
       prop.classes [ "w-96" ]
       prop.children [
@@ -18,9 +18,32 @@ type Components =
               Html.img [ prop.src cardLogic.img ]
             ]
             Daisy.cardBody [
-              prop.style [
-                style.alignItems.center
+              prop.style [ style.alignItems.center ]
+              prop.children [
+                Daisy.cardTitle cardLogic.title
+                Html.p cardLogic.ability
+                Html.p cardLogic.alternative
               ]
+            ]
+          ]
+        ]
+      ]
+    ]
+
+  [<ReactComponent>]
+  static member PickableCard(cardLogic, cardIsPicked) =
+    Html.div [
+      prop.classes [ "w-96" ]
+      prop.children [
+        Daisy.card [
+          card.bordered
+          card.full
+          prop.children [
+            Html.figure [
+              Html.img [ prop.src cardLogic.img ]
+            ]
+            Daisy.cardBody [
+              prop.style [ style.alignItems.center ]
               prop.children [
                 Daisy.cardTitle cardLogic.title
                 Html.p cardLogic.ability
@@ -40,32 +63,54 @@ type Components =
     ]
 
   [<ReactComponent>]
-  static member CardsHand(cards: Card List) =
-    let (cardPicked, chooseCard) = React.useState (0)
-    let whichCardWasPicked number _ = chooseCard (number)
-
+  static member CardsHand(cards: Card List, whichCardWasPicked) =
     Html.div [
-      Html.text (
-        if cardPicked > 0 then
-          $"Picked card: #{cardPicked}"
-        else
-          "Pick a card"
-      )
-      Html.div [
-        prop.style [
-          style.display.flex
-          style.flexDirection.row
-          style.flexWrap.nowrap
-        ]
-        prop.children [
-          for card in cards do
-            Components.Card(card, (whichCardWasPicked card.id))
-        ]
+      prop.style [
+        style.display.flex
+        style.flexDirection.row
+        style.flexWrap.nowrap
+      ]
+      prop.children [
+        for card in cards do
+          Components.PickableCard(card, (whichCardWasPicked card.id))
+      ]
+    ]
+
+  [<ReactComponent>]
+  static member CardsTableu(cards: Card List) =
+    Html.div [
+      prop.style [
+        style.display.flex
+        style.flexDirection.column
+        style.flexWrap.nowrap
+      ]
+      prop.children [
+        for card in cards do
+          Components.StaticCard(card)
       ]
     ]
 
   [<ReactComponent>]
   static member GameContainer() =
+    let (cardsInTableu, setCardsInTableu) = React.useState (List.empty)
+    let (cardPickedId, chooseCard) = React.useState (0)
+    let whichCardWasPicked number _ =
+      chooseCard(number)
+      let cardPicked =
+        Logic.exampleCards
+        |> List.find (fun c -> c.id = number)
+      setCardsInTableu(cardPicked :: cardsInTableu)
+
     Html.div [
-      Components.CardsHand(Logic.exampleCards)
+      Html.div [
+        Html.text (
+          if cardPickedId> 0 then
+            $"Picked card: #{cardPickedId}"
+          else
+            "Pick a card"
+        )
+        Components.CardsHand(Logic.exampleCards, whichCardWasPicked)
+        Html.text "Your stack of cards (newer ones show at the top)"
+        Components.CardsTableu(cardsInTableu)
+      ]
     ]
